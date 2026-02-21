df8b227ac99bdfe4d8733fb32716e052f9decc27
0Reab <0Reab@users.noreply.github.com>
2026-02-21 13:46:10 +0100

    method docs and return type anotation

--- board.py
+++ board.py
@@ -23,7 +23,8 @@ class Board:
             result += '\n'
         return result
 
-    def __init_squares(self):
+    def __init_squares(self) -> list:
+        '''Initialize pieces to home squares on the board'''
         rows = []
         square = []
 
@@ -36,10 +37,12 @@ class Board:
 
         return rows
 
-    def display_data(self):
+    def display_data(self) -> None:
+        '''Prints each board row in more detail'''
         print(''.join([f'{self.display_row_data(row)}\n' for row in self.board])) 
 
-    def display_row_data(self, row):
+    def display_row_data(self, row) -> str:
+        '''Helper method for display_data() compiles a string of board row information'''
         row -= 1
         s = ''
         for sqr in self.board[row]:
@@ -47,24 +50,29 @@ class Board:
             s += ' '.join(sqr.get_piece().get_data()) + '  '
         return s
 
-    def get_array_idx(self, square):
+    def get_array_idx(self, square) -> tuple[int, int]:
+        '''Square to board array indices [x,y]'''
         col, row = square.get_pos()
         return [row - 1, self.letters_to_idx[col]]
 
-    def get_horizontal_file(self, square):
+    def get_horizontal_file(self, square) -> list[Square]:
+        '''Get whole row of the input square'''
         row, _ = self.get_array_idx(square)
 
         return self.board[row]
 
-    def get_vertical_file(self, square):
+    def get_vertical_file(self, square) -> list[Square]:
+        '''Get whole column of the input square'''
         _, col = self.get_array_idx(square)
 
         return [ row[col] for row in self.board ]
 
-    def get_diags(self, square):
+    def get_diags(self, square) -> list[Square]:
+        '''Get whole diagonals of the input square'''
         return NotImplemented
 
-    def get_square(self, position):
+    def get_square(self, position) -> Square:
+        '''From notation (eg. E4) return square object from the board'''
         # row is number, col is letter E4
         col, row = position
         row -= 1 # adjust for 0 indexed array;
@@ -77,7 +85,8 @@ class Board:
         square = self.board[row][col_idx]
         return square
 
-    def get_moves(self, square):
+    def get_moves(self, square) -> list[Square]:
+        '''All possible piece moves of the given square'''
         moves = []
         piece = square.piece
 
