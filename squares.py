6eeb573536181064052538d189262ca8faf899e8
0Reab <0Reab@users.noreply.github.com>
2026-02-21 13:56:35 +0100

    method docs and return type anotation

--- squares.py
+++ squares.py
@@ -7,17 +7,23 @@ class Square:
         self.number = number 
         self.piece = piece
 
-    def get_piece(self):
+    def get_piece(self) -> Piece | None:
+        '''Get piece object of this square if piece.exists == True'''
         return self.piece if self.piece.exists else None
 
-    def get_pos(self):
+    def get_pos(self) -> tuple[str, int]:
+        '''Get square notation eg. -> ["E", 4]'''
         return [self.letter, self.number]
 
-    def set_piece(self, piece):
+    def set_piece(self, piece) -> None:
+        '''Update square with new piece'''
+        # mby do some checks here idek
         self.piece = piece
     
-    def clear(self):
+    def clear(self) -> None:
+        '''Clear square by instanciating new empty piece object'''
         self.piece = Piece(self.letter, self.number, captured=True)
 
-    def is_ocupied(self):
+    def is_ocupied(self) -> bool:
+        '''Check piece object of the square, does piece.exists == True'''
         return self.piece.exists
